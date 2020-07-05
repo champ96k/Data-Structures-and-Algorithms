4e77c1c4389bc76b6f88fe0e79bc90c84cc3f1ee
Tushar Nikam <champ96k@gmail.com>
2020-07-06 00:03:37 +0530

    Smart Pointer

---
+++ C++ Bootcamp/Section 9/main.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+class User{
+public:
+    User(){
+        cout << "User created\n";
+    }
+    ~User(){
+        cout << "User destroyed\n";
+    }
+    
+    void testFunc(){
+        cout << "I am a test function\n";
+    }
+};
+
+
+int main()
+{
+  
+    {
+//        unique_ptr<User> sam(new User()); // non fav. way of developers
+        
+        unique_ptr<User> sam = make_unique<User>();
+        sam->testFunc();
+        
+//        unique_ptr<User> samm = sam;  NOT ALLOWED
+        
+    }
+    
+    {
+//        shared_ptr<User> tim(new User());
+        shared_ptr<User> tim = make_shared<User>();
+        shared_ptr<User> timm = tim;
+    }
+    
+    
+    cout << "outside code\n";
+    
+    return 0;
+    
+}
