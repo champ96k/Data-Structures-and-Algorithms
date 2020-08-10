8597804b9cb8337a375b4d5c47fb2e798c5f22b2
Tushar Nikam <champ96k@gmail.com>
2020-08-10 11:32:08 +0530

    Mathematics Practice Programe

---
+++ C++ Programs/MinimumGift.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void findMinimumGiftOfEmployess(int _rank[],int nnn,int xxx) {
+
+  int sum[xxx],token2;
+  
+  for(token2=0; token2 <xxx; token2++) {
+    
+      sum[token2]=1;
+  }
+  
+  cout<<"Display: "<<endl;
+  
+  for(int i=0; i<<xxx; i++) {
+    cout<<sum[i]<<" "<<endl;
+  }
+  
+
+ 
+}
+
+int main() {
+  
+  int _testCases;
+  cin>>_testCases;
+  while(_testCases--) {
+    int _noOfEmployees;
+    cin>>_noOfEmployees;
+    int _rankOfEmp[_noOfEmployees];
+    
+    //Accept the rank of employees
+    for(int token1=0; token1<_noOfEmployees; token1++) {
+      cin>>_rankOfEmp[token1];
+    }
+    
+    int n = sizeof(_rankOfEmp)/sizeof(_rankOfEmp[0]);
+    
+    findMinimumGiftOfEmployess(_rankOfEmp, n,_noOfEmployees);
+   }   
+    
+  return 0;
+}
---
+++ Geeksforgeeks DSA Self Paced/Mathematics/Problems/ModularMultiInverse.cpp~
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+//Complete this function
+int modInverse(int a, int m)
+{   for(int i=0;i<m;i++){
+     if((a*i)%m==1)
+        return i;
+    }
+    return -1;
+    //Your code here
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--){
+		int a,m;
+		cin>>a>>m;
+		cout << modInverse(a, m)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
