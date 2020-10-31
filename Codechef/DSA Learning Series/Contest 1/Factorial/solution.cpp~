d4fd18f9c0784b7bca74f4735e0658d7b96fa9ca
Tushar Nikam <champ96k@gmail.com>
2020-10-31 10:18:09 +0530

    Hashing GFG assignment problem

---
+++ Codechef/DSA Learning Series/Contest 1/Factorial/solution.cpp~
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+    	long long n,count=0;  cin >> n;
+    	for(long long i=5;n/i>=1;i*=5)
+    	{
+    	    count += n/i;
+    	}
+    	cout << count << endl;
+    }
+	return 0;
+}
---
+++ Geeksforgeeks/Hashing/1. Separate chaining in Hashing/solution.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> separateChaining(int hashSize,int arr[],int sizeOfArray);
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int hashSize;
+	    cin>>hashSize;
+	    
+	    
+	    int sizeOfArray;
+	    cin>>sizeOfArray;
+	    int arr[sizeOfArray];
+	    
+	    for(int i=0;i<sizeOfArray;i++)
+	    cin>>arr[i];
+	    
+	    vector<vector<int>> hashTable;
+	    hashTable = (separateChaining( hashSize, arr, sizeOfArray));
+	    
+	    for(int i=0;i<hashTable.size();i++)
+	    {
+	        if(hashTable[i].size()>0)
+	        {
+	            cout<<i<<"->";
+	            for(int j=0;j<hashTable[i].size()-1;j++)
+	            {
+	                cout<<hashTable[i][j]<<"->";
+	            }
+	            cout<<hashTable[i][hashTable[i].size()-1];
+	            cout<<endl;
+	        }
+	    }
+	    
+	    
+	    
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+//Complete this function
+
+vector<vector<int>> separateChaining(int hashSize,int arr[],int sizeOfArray)
+{
+   //Your code here
+  vector<vector<int>> ans(hashSize);
+
+   for(int i=0;i<sizeOfArray;i++) {
+      
+        int key=arr[i]%hashSize;
+        ans[key].push_back(arr[i]);
+   }
+   
+   return ans;
+   
+}
