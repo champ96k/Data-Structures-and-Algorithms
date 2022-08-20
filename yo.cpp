eaf403480efefd9ea0d3c3406850394b0c1a6b0d
Tushar Nikam <champ96k@gmail.com>
2022-08-20 12:26:49 +0530

    sublime setup added

---
+++ yo.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	//from getting input from input.tex
+	freopen("input.txt","r",stdin);
+	//from writing out from output.tex
+	freopen("output.txt","w",stdout);
+#endif
+
+
+	int t;
+	cin>>t;
+
+	while(t--) {
+		cout<<t<<endl;
+	}
+	
+	return 0;
+}
