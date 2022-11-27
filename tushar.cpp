918d344a8bdf30c6c85dffebb81dfb4df7a2d9d1
Tushar Nikam <champ96k@gmail.com>
2022-11-27 14:54:50 +0530

    sublime setup done

--- tushar.cpp
+++ tushar.cpp
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-void solve(); 
+
 int main() 
 { 
-	ios_base::sync_with_stdio(false); 
-	cin.tie(NULL); 
-
+	
 #ifndef ONLINE_JUDGE 
 	freopen("input.txt", "r", stdin); 
-	freopen("error.txt", "w", stderr); 
 	freopen("output.txt", "w", stdout); 
 #endif 
 
-	int t = 1; 
-	/*is Single Test case?*/ cin >> t; 
+	int t; 
+	cin>>t;
+
 	while (t--) { 
-		solve(); 
-		cout << "\n"; 
+
+		cout << "Hey there!"<<endl; 
 	} 
 
 	cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
 	return 0; 
-} 
-void solve() 
-{ 
 } 
